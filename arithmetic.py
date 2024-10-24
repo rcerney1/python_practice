8f22a03c4f19834bf2f482ad92bc2c33f668c255
rcerney <ryancerney1@gmail.com>
2024-10-24 13:57:02 -0700

    first commit

---
+++ arithmetic.py
@@ -0,0 +1,28 @@
+# arithmetic = + addition
+#              - subtraction
+#              * multiply
+#              / divide
+#              / / integer division (returns an integer rather than a float)
+#              % modolo
+
+friends = 5
+
+friends = friends + 3 
+print('------')
+print("friends:", friends)
+print("------")
+
+friends += 1
+
+print("friends:", friends)
+print("------")
+
+friends = friends // 2
+
+print("friends:", friends)
+print("------")
+
+friends *= 2 
+
+print("friends:", friends)
+print("------")
---
+++ demo.py
@@ -0,0 +1,16 @@
+#this will be my first python program
+print("This is a print statement")
+
+#variables
+#strings, integers, floats, booleans, etc.
+fullName = "Ryan Cerney"
+age = 30
+gpa = 3.8
+#booleans must be capitalized
+isStudent = True
+
+#printing f statements
+print (f"Hello, {fullName}")
+print (f"You are {age} years old")
+print(f"Your gpa is {gpa}")
+print(f"Are you a student?: {isStudent}")
